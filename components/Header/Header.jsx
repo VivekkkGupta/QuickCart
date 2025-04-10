d61cd10c96726208ae5d6c2d54e6f19853c098e9
Vivek <gupta1035@gmail.com>
2025-04-10 20:31:05 +0530

    form data changes

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -9,7 +9,7 @@ import CartIconButton from "./CartIconButton";
 
 function Header() {
     return (
-        <header className="top-0 z-50 w-full border-b bg-white sticky top-0">
+        <header className="top-0 z-50 w-full border-b bg-white sticky">
             <div className="relative w-full h-15 flex items-center justify-between overflow-hidden py-2 px-4 border-b max-w-[1240px] mx-auto">
                 <LogoSection />
                 <NavLinks /> {/* No need to pass pathname */}
