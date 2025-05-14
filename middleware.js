970eb401bbff4a075b1f5a0a051570282bdbb50e
Vivek Kumar Gupta <40853271+VivekkkGupta@users.noreply.github.com>
2025-05-14 06:45:22 +0000

    protected-myorders-address

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/cart(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/cart(.*)","/my-orders","/address"]);
 
 export default clerkMiddleware(async(auth, req) => {
   const { userId } = await auth(); // Get userId
