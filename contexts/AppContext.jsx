fa8b98cd5d3f9a86df7d340972b3c84a4d362ef6
Vivek Kumar Gupta <40853271+VivekkkGupta@users.noreply.github.com>
2025-06-09 07:50:49 +0000

    changes in Auth

--- contexts/AppContext.jsx
+++ contexts/AppContext.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useCallback, use, useMemo } from "react";
